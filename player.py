155002700ee2a3837adcf7f5659de1e014eddc19
Ignas <zizys.ignas@gmail.com>
2025-02-02 14:41:54 +0200

    session with Dainius result

--- player.py
+++ player.py
@@ -1,10 +1,23 @@
-from deck import Card, Deck
+from deck import Deck
 #A player needs to:
 
-#Hold a stack of cards
+
+
+#Draw a card
 #Play a card
 #Collect won cards
 class Player:
     def __init__(self, name):
         self.name = name
-        self.cards = []
+        self.cards = [] #Hold a stack of cards
+
+    def draw(self):
+        
+        while len(self.cards) <= 26:
+            self.cards.append(Deck.deal())
+        return self.cards
+    def play(self):
+        return self.cards.pop()
+    
+ 
+
